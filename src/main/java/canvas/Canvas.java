2e89932fc3d16bfc18e78ccff5fe07ca46c659bd
Luis Cesar Maiaru <lmaiaru@proveedor.personal.com.ar>
2017-06-28 21:39:23 +0100

    Initial test and implementation of Canvas constructor.

--- src/main/java/canvas/Canvas.java
+++ src/main/java/canvas/Canvas.java
@@ -1,5 +1,65 @@
 package canvas;
 
+import java.util.Arrays;
+
 public class Canvas {
 
-}
+	private char[][] canvas;
+	private char blank = ' ';
+	private char hMark = '-';
+	private char vMark = '|';
+	private int horizontalDimension;
+	private int verticalDimension;
+	private String horizontalBorder;
+	private String verticalBorder;
+
+	public Canvas(int horizontalDimension, int verticalDimension) {
+		canvas = new char[verticalDimension][horizontalDimension];
+		this.horizontalDimension = horizontalDimension;
+		this.verticalDimension = verticalDimension;
+		initCanvas();
+	}
+	
+	private void initCanvas() {
+		for (int i = 0; i < this.verticalDimension; i++) {
+			for (int j = 0; j < this.horizontalDimension; j++) {
+				canvas[i][j] = blank;
+			}
+		} // for
+		createBorder();
+	} // initCanvas
+
+	private void createBorder() {
+		final char[] hArray = new char[horizontalDimension + 2];
+		Arrays.fill(hArray, hMark);
+	    horizontalBorder = new String(hArray);
+		final char[] vArray = new char[horizontalDimension];
+		Arrays.fill(vArray, blank);
+	    verticalBorder = vMark + new String(vArray) + vMark;		
+	}
+
+	public char getBlank() {
+		return blank;
+	}
+
+	public void setBlank(char blank) {
+		this.blank = blank;
+	}
+
+	public String getHorizontalBorder() {
+		return horizontalBorder;
+	}
+
+	public void setHorizontalBorder(String horizontalBorder) {
+		this.horizontalBorder = horizontalBorder;
+	}
+
+	public String getVerticalBorder() {
+		return verticalBorder;
+	}
+
+	public void setVerticalBorder(String verticalBorder) {
+		this.verticalBorder = verticalBorder;
+	}
+
+} // class Canvas
--- src/test/java/canvas/CanvasTest.java
+++ src/test/java/canvas/CanvasTest.java
@@ -1,5 +1,23 @@
 package canvas;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 public class CanvasTest {
 
-}
+	@Test
+	public void testConstructor() {
+		int horizontalDimension = 4;
+		int verticalDimension = 4;
+		Canvas canvas = new Canvas(horizontalDimension, verticalDimension);
+		String horizontalBorder = canvas.getHorizontalBorder();
+		String verticalBorder = canvas.getVerticalBorder();
+		String expectedHorizontalBorder = "------";
+		String expectedVerticalBorder = "|    |";
+		assertTrue(expectedHorizontalBorder.equals(horizontalBorder));
+		assertTrue(expectedVerticalBorder.equals(verticalBorder));
+	} // testConstructor
+	
+} // class CanvasTest
